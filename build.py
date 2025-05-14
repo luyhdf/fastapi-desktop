0aca0c90cf3a608ffc95dd94d40d968416625cc3
luyhdf <461587276@qq.com>
2025-05-14 14:31:51 +0800

    build

---
+++ build.py
@@ -0,0 +1,16 @@
+from PyInstaller import __main__ as pyi
+
+
+params = [
+    # 打包为单文件
+    '-F', 
+    # static目录纳入打包
+    '--add-data', 'static;static',
+    # 每次打包前清楚build 和 dist目录    
+    '--clean',
+    # 无需用户确认
+    '--noconfirm',
+    'client.py'
+]
+# pyinstaller -F main.py
+pyi.run(params)
