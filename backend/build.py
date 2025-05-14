c57e3c88fb0da2dc18956d6ffc6bb741fb104ae8
luyhdf <461587276@qq.com>
2025-05-14 16:10:52 +0800

    build 输出至/dist目录

--- backend/build.py
+++ backend/build.py
@@ -6,10 +6,16 @@ from PyInstaller import __main__ as pyi
 import i2cpy
 i2cpy_path = os.path.dirname(i2cpy.__file__)
 
+# 确保输出目录存在
+dist_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'dist'))
+os.makedirs(dist_dir, exist_ok=True)
+
 # 构建参数
 params = [
     # 打包为单文件
     '-F',
+    # 设置输出目录
+    '--distpath', dist_dir,
     # 添加数据文件
     '--add-data', f'{os.path.join(i2cpy_path, "driver")};i2cpy/driver',
     '--add-data', 'static;static',
